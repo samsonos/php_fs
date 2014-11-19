01bc7b3dc382be10c0b340e39c0fc5b9a2af063c
onysko <pavlo.onysko@gmail.com>
2014-11-19 19:05:12 +0200

    module connector added

---
+++ src/FileSystemConnector.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: onysko
+ * Date: 19.11.2014
+ * Time: 19:04
+ */
+
+namespace samson\fs;
+
+
+class FileSystemConnector extends \samson\core\CompressableExternalModule{
+    /** Идентификатор модуля */
+    protected $id = 'samson_fs';
+} 
