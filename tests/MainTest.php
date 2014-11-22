bcac3f440ffc87ad4c38b5b1406adfe73ebf54fa
Vitaly Egorov <egorov@samsonos.com>
2014-11-22 17:57:57 +0200

    fixing tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -13,11 +13,7 @@ class EventTest extends \PHPUnit_Framework_TestCase
     public function testInitialize()
     {
         // Create instance
-        $fileService = new FileService(__DIR__.'../');
-
-        // Create local file service instance
-        new \samson\fs\LocalFileService();
-
+        $fileService = new FileService(__DIR__.'../');  
 
         // Initialize method
         $fileService->init(array(''));
