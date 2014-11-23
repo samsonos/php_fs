e6f0a62fd848fcc3e88b9b80de535782c137dd37
Vitaly Egorov <egorov@samsonos.com>
2014-11-23 16:37:16 +0200

    Added tests

--- tests/MainTest.php
+++ tests/MainTest.php
@@ -75,4 +75,61 @@ class EventTest extends \PHPUnit_Framework_TestCase
         // Perform test
         $this->assertEquals('123', $data, 'File service writing failed');
     }
+
+    /** Test file service deleting */
+    public function testDelete()
+    {
+        // Get instance using services factory as error will signal other way
+        $this->fileService = \samson\core\Service::getInstance('samson\fs\FileService');
+
+        // Create temporary file
+        $path = tempnam(sys_get_temp_dir(), 'test');
+
+        // Delete temporary file
+        $this->fileService->delete('123', $path);
+
+        // Perform test
+        $this->assertFileExists($path, 'File service deleting failed');
+    }
+
+    /** Test file service existing */
+    public function testExists()
+    {
+        // Get instance using services factory as error will signal other way
+        $this->fileService = \samson\core\Service::getInstance('samson\fs\FileService');
+
+        // Create temporary file
+        $path = tempnam(sys_get_temp_dir(), 'test');
+
+        // Write data to temporary file
+        $exists = $this->fileService->exists($path);
+
+        // Perform test
+        $this->assertEquals(true, $exists, 'File service exists failed');
+    }
+
+    /** Test file service moving */
+    public function testMove()
+    {
+        // Get instance using services factory as error will signal other way
+        $this->fileService = \samson\core\Service::getInstance('samson\fs\FileService');
+
+        // Create temporary file
+        $path = tempnam(sys_get_temp_dir(), 'test');
+
+        // Create test dir
+        $testDir = sys_get_temp_dir().'testDir/';
+        mkdir($testDir, 0777);
+
+        // Move file to a new dir
+        $newPath = $this->fileService->move($path, basename($path), $testDir);
+
+        // Test files existence
+        $existsMoved = $this->fileService->exists($newPath);
+        $existsOld = $this->fileService->exists($newPath);
+
+        // Perform test
+        $this->assertEquals(true, $existsMoved, 'File service move failed - Moved file not found');
+        $this->assertEquals(false, $existsOld, 'File service move failed - Original file is not deleted');
+    }
 }
